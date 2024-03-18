8bf5e3c1e7bdf9ece2b54be0a0d44e543842966b
dowonseo <dsa3182@gmail.com>
2024-03-18 14:19:39 +0900

    Rewrite JS Hello API

--- 2-hello-api/hello-api-js/handler.js
+++ 2-hello-api/hello-api-js/handler.js
@@ -1,16 +1,12 @@
 "use strict";
 
 module.exports.hello = async (event) => {
+    if (!event.queryStringParameters || !event.queryStringParameters.name) {
+        return { statusCode: 404, body: "not found" };
+    }
     return {
         statusCode: 200,
-        body: JSON.stringify(
-            {
-                message: "Go Serverless v1.0! Your function executed successfully!",
-                input: event,
-            },
-            null,
-            2
-        ),
+        body: `hello ${event.queryStringParameters.name}`,
     };
 
     // Use this code if you don't use the http event with the LAMBDA-PROXY integration
